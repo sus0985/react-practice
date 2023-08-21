349aa077d6f8e2e27489941b2f954eff87c2d1dd
edd <edd@vlending.co.kr>
2023-08-21 21:22:28 +0900

    생활코딩 React State와 props에 따라 render 함수 재호출

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            mode: 'read',
             subject: {title: 'WEBBBBB', sub: 'Sub title!!!!!'},
+            welcome: {title:'Welcome', desc:'Hello React!!'},
             contents: [
                 {id: 1, title: 'HTML', desc: 'HTML is for information'},
                 {id: 2, title: 'CSS', desc: 'CSS is for design'},
@@ -17,16 +19,28 @@ class App extends Component {
         }
     }
 
+    // props나 state가 바뀌면 가지고있는 컴포넌트에서 render 함수가 다시 호출됨
+    // render 함수가 다시 호출되면서 하위 컴포넌트도 전부 render가 다시 호출
+    // 즉, 자기 자신과 자식의 컴포넌트가 다시 그려짐
     render() {
+        console.log('App render');
+        let _title, _desc = null;
+
+        if (this.state.mode === 'welcome') {
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if (this.state.mode ==='read') {
+            _title = this.state.contents[0].title;
+            _desc = this.state.contents[0].desc;
+        }
         return (
             <div className="App">
                 <Subject
                     title={this.state.subject.title}
                     sub={this.state.subject.sub}>
                 </Subject>
-                <Subject title="React" sub="For UI"></Subject>
                 <TOC data={this.state.contents}></TOC>
-                <Content title="HTMLLLL" desc="HTML is HyperText Markup Language."></Content>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         );
     }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,9 +2,10 @@ import React, {Component} from "react";
 
 class Subject extends Component {
     render() {
+        console.log('Subject render')
         return ( // 하나의 최상위 태그로 시작해야함
             <header>
-                <h1>{this.props.title}</h1>
+                <h1><a href="/">{this.props.title}</a></h1>
                 {this.props.sub}
             </header>
         );
