3b4baf9d777ffb5da1ce92ccb8abec3f3ad272cc
edd <edd@vlending.co.kr>
2023-08-21 21:11:09 +0900

    생활코딩 React State 추가

--- src/App.js
+++ src/App.js
@@ -5,12 +5,27 @@ import Content from "./components/Content"
 import Subject from "./components/Subject"
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            subject: {title: 'WEBBBBB', sub: 'Sub title!!!!!'},
+            contents: [
+                {id: 1, title: 'HTML', desc: 'HTML is for information'},
+                {id: 2, title: 'CSS', desc: 'CSS is for design'},
+                {id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive'}
+            ]
+        }
+    }
+
     render() {
         return (
             <div className="App">
-                <Subject title="WEBBBBBB" sub="Sub title!!"></Subject>
+                <Subject
+                    title={this.state.subject.title}
+                    sub={this.state.subject.sub}>
+                </Subject>
                 <Subject title="React" sub="For UI"></Subject>
-                <TOC></TOC>
+                <TOC data={this.state.contents}></TOC>
                 <Content title="HTMLLLL" desc="HTML is HyperText Markup Language."></Content>
             </div>
         );
